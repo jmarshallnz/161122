99a6cb700783d307c3d28f9c1b3799bab42986cd
Jonathan Marshall <jmarshall@never.you.mind>
2022-07-22 10:00:17 +1200

    update solutions to A2 and A3 for 2022

--- solutions/solA02.R
+++ solutions/solA02.R
@@ -8,129 +8,148 @@ data(quakes)
 ggplot(data=quakes) +
   geom_point(mapping=aes(x=long, y=lat))
 
-# Let's try some other geometries. First: geom_bin2d
+# We can try geom_bin2d instead to see what that does:
 ggplot(data=quakes) +
   geom_bin2d(mapping=aes(x=long, y=lat))
 
-# A 2d density
+# this is showing us that a bunch of earthquakes occur along
+# a line around 180 degrees longitude.
 ggplot(data=quakes) +
   geom_density2d(mapping=aes(x=long, y=lat))
 
-# Showing two geometries
+# This is done by popping a little blob of 'jelly' on each of
+# the 1000 points. Where points are close to each other the jelly
+# accumulates into mountains.
 ggplot(data=quakes) +
-  geom_point(mapping=aes(x=long, y=lat), alpha=0.5) +
-  geom_density2d(mapping=aes(x=long, y=lat),
-                 linetype = 'longdash',
-                 col='red')
+  geom_point(mapping=aes(x=long, y=lat), alpha=0.3) +
+  geom_density2d(mapping=aes(x=long, y=lat), col='dark red', size=0.1)
 
-# Finding help on the aesthetics (e.g. linetypes)
+# We can change the colour with `col` or `color` or `colour`. The width
+# of the lines are specified with `size`.
+
+# The vignette can tell us more:
 vignette("ggplot2-specs")
 
-# Altering the amount of smoothing that the density does via
-# the adjust parameter. Smaller than one smoothes less:
+# So to use dashed lines:
 ggplot(data=quakes) +
-  geom_point(mapping=aes(x=long, y=lat), alpha=0.5) +
-  geom_density2d(mapping=aes(x=long, y=lat),
-                 linetype = 'solid',
-                 col='red',
-                 adjust = 0.5)
+  geom_point(mapping=aes(x=long, y=lat), alpha=0.3) +
+  geom_density2d(mapping=aes(x=long, y=lat), col='dark red', size=0.5,
+                 linetype='dashed')
 
-# Larger than one smooths more:
+
+# We can also adjust the amount of 'smoothing' that is being
+# applied when using the density geometry:
+# The default smooths based on some criteria that tends to work
+# well but is nonetheless somewhat arbitrary.
 ggplot(data=quakes) +
-  geom_point(mapping=aes(x=long, y=lat), alpha=0.5) +
-  geom_density2d(mapping=aes(x=long, y=lat),
-                 linetype = 'solid',
-                 col='red',
-                 adjust = 2)
+  geom_point(mapping=aes(x=long, y=lat), alpha=0.3) +
+  geom_density2d(mapping=aes(x=long, y=lat), col='dark red', size=0.5,
+                 linetype='solid')
 
-# We can tune the amount of smoothing that we want.
-# Choosing a small value of adjust will mean we have less
-# smoothing, so the smoothed density will be closer to the
-# data that we have built it from. But, when a new dataset
-# comes along, the density that we build from that data might
-# look quite a bit different to this one.
+# We can adjust the amount of smoothing via the `adjust` parameter.
+# Default is 1, values less than one will smooth less, and values
+# greater than one will smooth more.
 
-# Choosing a large value of adjust will smooth more. The density
-# will not reflect our data quite as well (as we smooth it too
-# much), but when we do the same thing on a new dataset, we're
-# likely to get a very similar smoothed density. This is the
-# bias-variance tradeoff: By reducing bias (smoothing less) we
-# increase variance (between data sets). Alternatively we can
-# reduce variance (so different datasets produce similar results)
-# but as a result each one will be more biased (doesn't fit a
-# particularly dataset very well).
-
+# Less smoothing:
 ggplot(data=quakes) +
-  geom_point(mapping=aes(x=long, y=lat), alpha=0.5) +
-  geom_density2d(mapping=aes(x=long, y=lat),
-                 linetype = 'solid',
-                 col='red',
-                 adjust = 1) +
-  labs(x = "Longitude", y = "Latitude",
-       title = "There are lots of earthquakes along a ridge around 180 degrees Longitude")
+  geom_point(mapping=aes(x=long, y=lat), alpha=0.3) +
+  geom_density2d(mapping=aes(x=long, y=lat), col='dark red', size=0.5,
+                 linetype='solid', adjust=0.5)
 
-# Having a look at a different plot:
+# More smoothing:
+ggplot(data=quakes) +
+  geom_point(mapping=aes(x=long, y=lat), alpha=0.3) +
+  geom_density2d(mapping=aes(x=long, y=lat), col='dark red', size=0.5,
+                 linetype='solid', adjust=2)
+
+# When we smooth only a little (i.e. adjust < 1) then our density estimate
+# follows our data better, capturing detail.
+
+# When we smooth a lot (adjust > 1) then our density estimate starts 'smooshing'
+# the data so we're not really capturing the data intricacies or detail.
+
+# When considering sample to sample variation, if we smooth only a little,
+# then the density estimate from the two samples may differ (potentially quite
+# a bit) as the two samples will differ a little bit.
+
+# If we smooth a lot, then our sample to sample variation of the density
+# will be expected to be small, because we're smoothing a lot, so any differences
+# will be smoothed over.
+
+# So there's a trade-off: Smoothing a little bit gets us closer to the
+# data we have in front of us. But, it might mean when we get a new sample
+# our 'trend' from the density will differ a bunch.
+
+# Smoothing a lot will leave us further away from the data in front of us,
+# (we're not getting the detail) but the sample to sample variation will
+# be low.
+
+# This is known as the Bias-Variance trade-off.
+
+# Tidy up the axis labels and go back to the default level of smoothing:
+ggplot(data=quakes) +
+  geom_point(mapping=aes(x=long, y=lat), alpha=0.3) +
+  geom_density2d(mapping=aes(x=long, y=lat), col='dark red', size=0.5,
+                 linetype='solid', adjust=1) +
+  labs(x = "Longitude", y="Latitude",
+       title = "Earthquakes around Fiji",
+       subtitle = "Many earthquakes occur along a ridge around Longitude 180")
+
+# Improving a plot:
 ggplot(data=quakes) +
   geom_point(mapping=aes(x=mag, y=stations))
 
-# These data are rounded, so there may be overplotting where
-# more than one datapoint are identical in both x and y so
-# show up in exactly the same spot. We could look for this
-# by using some transparency:
+# We notice that the magnitude variable has been rounded. This has led
+# to overplotting.
+ggplot(data=quakes) +
+  geom_jitter(mapping=aes(x=mag, y=stations),
+              width = 0.06, height = 0)
+
+# Jitter adds some random noise to the data. useful if the data has
+# been rounded (artificially). Nonetheless, this is altering the
+# data, so should be used with caution.
+
+# The option to deal with overplotting is to use transparency:
 ggplot(data=quakes) +
   geom_point(mapping=aes(x=mag, y=stations),
              alpha = 0.2)
 
-# We can also do this by jittering the data points: Adding
-# a small amount of random noise to the data to get rid of
-# the rounding (note that this is just made up though!)
+# Or you could combine them:
 ggplot(data=quakes) +
   geom_jitter(mapping=aes(x=mag, y=stations),
-              alpha=0.3,
-              width = 0.06, height = 0)
+              width = 0.04, height = 0, alpha=0.3)
 
-# We could try adding a density2d like above:
+# What about adding some smoothing to get the underlying trend?
+
+# We could try density2d again: This shows the areas of the x/y
+# grid that have the most points in them.
 ggplot(data=quakes) +
   geom_jitter(mapping=aes(x=mag, y=stations),
-              alpha=0.3,
-              width = 0.04, height = 0) +
-  geom_density2d(mapping = aes(x=mag, y=stations))
+              width = 0.04, height = 0, alpha=0.3) +
+  geom_density2d(mapping=aes(x=mag, y=stations))
 
-# Or maybe geom_smooth() - this looks better as it
-# helps us tell the story: Increasing magnitude leads
-# to more stations detecting the earthquake.
+# In this case we probably want the trend in the data instead,
+# which geom_smooth() gives us:
 ggplot(data=quakes) +
   geom_jitter(mapping=aes(x=mag, y=stations),
-              alpha=0.3,
-              width = 0.04, height = 0) +
-  geom_smooth(mapping = aes(x=mag, y=stations))
+              width = 0.04, height = 0, alpha=0.3) +
+  geom_smooth(mapping=aes(x=mag, y=stations))
 
-# Playing around with the smooth (how smooth?)
+# Just like with the density2d smoother, we can mess about with
+# how smooth the geom_smooth is.
+
+# To do this we switch the type of smoother to a `loess` and
+# adjust the `span`:
 ggplot(data=quakes) +
   geom_jitter(mapping=aes(x=mag, y=stations),
-              alpha=0.3,
-              width = 0.04, height = 0) +
-  geom_smooth(mapping = aes(x=mag, y=stations),
-              method = 'loess', span = 0.35)
-
-# Another example of the bias-variance tradeoff: reducing
-# span means you smooth less which means the curve is closer
-# to the data trend. But, a new dataset will have a different
-# smooth curve so the variation between datasets increases.
-
-# Increasing span means you smooth more which means the curve
-# doesn't fit the data as well. But, a new dataset smoothed
-# the same way will result in a very similar smooth trend.
-# i.e. variation between datasets is reduced, but bias for
-# a particular dataset increases.
+              width = 0.04, height = 0, alpha=0.3) +
+  geom_smooth(mapping=aes(x=mag, y=stations),
+              method='loess', span=0.4)
 
 ggplot(data=quakes) +
   geom_jitter(mapping=aes(x=mag, y=stations),
-              alpha=0.3,
-              width = 0.04, height = 0) +
-  geom_smooth(mapping = aes(x=mag, y=stations),
-              method = 'loess', span = 0.6) +
-  labs(x = "Magnitude of earthquake",
-       y = "Number of stations that detect the earthquake",
-       title = "Bigger earthquakes are felt more widely")
-
+              width = 0.04, height = 0, alpha=0.3) +
+  geom_smooth(mapping=aes(x=mag, y=stations),
+              method='loess', span=0.4) +
+  labs(x="Magnitude", y="Number of stations",
+       title="Larger earthquakes are detected at more stations")
--- solutions/solA03.R
+++ solutions/solA03.R
@@ -6,114 +6,154 @@ data(quakes)
 ggplot(data=quakes) +
   geom_histogram(mapping=aes(x=mag))
 
-# This distribution shows that most quakes have magnitude 4-5, so the measure of center
-# would be around 4.5 magnitude (the mode). The spread is from 4 up to 6.5 (the range).
-# And the shape is skew to the right, in that most data is bunched up on the left, but
-# the 'tail' of large magnitude quakes goes out to the right.
+# Let's try varying the number of bins:
+ggplot(data=quakes) +
+  geom_histogram(mapping=aes(x=mag), bins=20)
 
-# 30 bins is the default. We can change that with bins:
+# This is a very different shape than with 30 bins!
+# 25 bins gives another again:
 ggplot(data=quakes) +
-  geom_histogram(mapping=aes(x=mag), bins=10)
-# Same conclusion from this one, but the display looks courser. The 'chunkiness' of
-# the plot is very apparent.
-ggplot(data=quakes) +
-  geom_histogram(mapping=aes(x=mag), bins=17)
-# With these data, the number of bins makes the plot look quite different!
+  geom_histogram(mapping=aes(x=mag), bins=25)
 
-# The reason is that the data here are rounded.
+# Let's see why this is happening by adding a 'data rug' to the plot.
 ggplot(data=quakes) +
-  geom_histogram(mapping=aes(x=mag), bins=30, alpha=0.2, col='black') +
-  geom_rug(mapping=aes(x=mag), col='red')
-# The rug shows us where the data are, so we can get a feel for why the
-# histogram is responding the way it is: As the data are rounded, some of the bins
-# consist of the quakes from two different magnitudes:
-ggplot(data=quakes) +
-  geom_histogram(mapping=aes(x=mag), bins=20, alpha=0.2, col='black') +
-  geom_rug(mapping=aes(x=mag), col='red')
-# e.g the tall spike here has magnitude 4.5 and 4.6 quakes contributing.
+  geom_histogram(mapping=aes(x=mag)) +
+  geom_rug(mapping=aes(x=mag))
 
-# and some consist of bins with no data:
-ggplot(data=quakes) +
-  geom_histogram(mapping=aes(x=mag), bins=30, alpha=0.2, col='black') +
-  geom_rug(mapping=aes(x=mag), col='red')
-# such as the bin between 4.0 and 4.1
+# We can clearly see here from the rug, that our observations are rounded,
+# appearing only at every 0.1. So the bar that starts a little higher than 4.0
+# and stops a little bit before 4.1 contains no observations!
 
-# An alternative is to specify the bin width and then the start of the bin.
+# What about 20 bins?
 ggplot(data=quakes) +
-  geom_histogram(mapping=aes(x=mag), binwidth = 0.2, boundary = 0.05, alpha=0.2, col='black') +
-  geom_rug(mapping=aes(x=mag), col='red')
+  geom_histogram(mapping=aes(x=mag), bins=20, fill='orange', col='black') +
+  geom_rug(mapping=aes(x=mag))
 
-ggplot(data=quakes) +
-  geom_histogram(mapping=aes(x=mag), binwidth = 0.1, boundary = 0.05, alpha=0.2, col='black') +
-  geom_rug(mapping=aes(x=mag), col='red')
+# We can see what is happening here - the very tall bar is covering observations
+# of magnitude 4.5 and 4.6 so is counting them both. Whereas most other bars are
+# only counting observations ta a single value.
 
-# The density plot gets rid of some of the problems with the histogram. It can be
-# thought of as a smooth histogram:
-ggplot(data=quakes) +
-  geom_density(mapping=aes(x=mag)) +
-  geom_rug(mapping=aes(x=mag), col='red')
+# This is a consequence of the rounding. When data are rounded, changing the number
+# of bins in a histogram can dramatically alter the shape. So generally, a histogram
+# isn't ideal for these data.
 
-# Can change the amount of smoothness: adjust > 1 smooths more:
+# However, we can work around this by specifying the bins so that they align
+# with the rounding in the data. We can do this by specifying the boundary between
+# bins to be in-between our observations (e.g. we want them to be half way between
+# 4.0 and 4.1, so we want them at 4.05 for example).
+# We then specify the binwidth (rather than number of bins) so that our bins all|
+# start in-between observations:
 ggplot(data=quakes) +
-  geom_density(mapping=aes(x=mag), adjust=4) +
-  geom_rug(mapping=aes(x=mag), col='red')
-# This is probably oversmoothed.
-#
-# adjust < 1 smooths less:
+  geom_histogram(mapping=aes(x=mag),
+                 fill='orange',
+                 col='black',
+                 binwidth=0.2,
+                 boundary=0.05,
+                 ) +
+  geom_rug(mapping=aes(x=mag))
+
+# Another option (often better!) is a kernel density estimate plot, or density plot.
+# This can be thought of as a smooth version of a histogram:
+ggplot(data=quakes) +
+  geom_density(mapping=aes(x=mag))
+
+# This looks like it might be slightly oversmoothed? We can alter the amount of
+# smoothing by using the `adjust` parameter:
+
+# A value lower than 1 will smooth less. 0.3 is certainly not enough smoothing
+# it is massively undersmoothed, but it does show how the kernel density is
+# constructed (by placing little 'blobs' centered at each `mag` data point, then
+# accummulating these 'blobs' up into hills).
 ggplot(data=quakes) +
   geom_density(mapping=aes(x=mag), adjust=0.3) +
-  geom_rug(mapping=aes(x=mag), col='red')
-# This is undersmoothed way too much - but, it helps us see how the density
-# plot is built up.
-ggplot(data=quakes) +
-  geom_density(mapping=aes(x=mag), adjust=0.6) +
-  geom_rug(mapping=aes(x=mag), col='red')
-# Even with adjust=0.6 we're probably still undersmoothing.
-# Typically the default (adjust=1) works well for most datasets.
+  geom_rug(mapping=aes(x=mag))
 
+# We might want a bit more smoothing than that: Let's try something larger than 1:
+# This is probably oversmoothed! We're losing detail now - there's just one hill?
 ggplot(data=quakes) +
-  geom_density(mapping=aes(x=mag), adjust=0.6, col='darkgreen', fill='pink', alpha=0.5) +
-  geom_rug(mapping=aes(x=mag), col='red')
+  geom_density(mapping=aes(x=mag), adjust=2) +
+  geom_rug(mapping=aes(x=mag))
 
-# The boxplot plots the minimum, lower quartile, median, upperquartile and maximum.
-# The five number summary.
+# Let's try 0.8 - this is still capturing the key trend (most common for earthquakes
+# to be around 4.5 magnitude, most occur between 4 and 5.5, a few higher around 6-6.5)
+ggplot(data=quakes) +
+  geom_density(mapping=aes(x=mag), adjust=0.8) +
+  geom_rug(mapping=aes(x=mag))
+
+# This distribution looks a bit skew to the right (i.e. longer tail on the right hand
+# side of the main data center), but this could just be due to there being no earthquakes
+# of magnitude less than 4 in the data. Presumably these exist, so the data have been
+# truncated.
+
+# We could also try a boxplot: A boxplot shows the 5 number summary (min, lower quartile,
+# median, upper quartile, max)
 ggplot(data=quakes) +
   geom_boxplot(mapping=aes(x=mag))
-# From here, we'd conclude center (median)  is at 4.6, and half the data are within the box
-# between 4.3 and 4.9. We get shape information from where the box is (and maybe where
-# the median is within the box).
+
+# Conclusion here is roughly the same! Most earthquakes are around 4.6 or so. Around
+# half are between the quartiles (inside the box) between 4.3 and 4.9. A quarter
+# to the left of the box (below 4.3) and a quarter to the right of the box (above 4.9)
+
+# Again this looks to be a bit skew to the right, however we notice that the median
+# is actually bang in the middle of the box. Again, remember that these data are likely
+# truncated at 4, so we're not getting a true representation here as we don't have
+# all possible magnitudes.
+
+# The observations being singled out on the right are potential outliers or extreme values.
+# It's somewhat arbitrary. What happens is the software takes the length of the box
+# (inter-quartile range) stretches it by 50% (i.e. 1.5*iqr) and takes that distance above
+# and below the box. Anything past those bounds are potential outliers or extreme values.
+
+# Boxplot and density colouring:
 ggplot(data=quakes) +
-  geom_boxplot(mapping=aes(x=mag), col='blue', fill='orange')
+  geom_boxplot(mapping=aes(x=mag), col='orange', fill='green3')
+
+ggplot(data=quakes) +
+  geom_density(mapping=aes(x=mag), col='orange', fill='green3', alpha=0.4)
+
+# We can turn off the line for the density by specifying col=NA:
+ggplot(data=quakes) +
+  geom_density(mapping=aes(x=mag), col=NA, fill='green3', alpha=0.4)
 
 # Let's have a look at depth:
 ggplot(data=quakes) +
-  geom_histogram(mapping=aes(x=depth))
+  geom_histogram(mapping=aes(x=depth), bins=20)
+
 ggplot(data=quakes) +
-  geom_density(mapping=aes(x=depth))
+  geom_density(mapping=aes(x=depth), adjust=0.8) +
+  geom_rug(mapping=aes(x=depth))
+
+# The histogram and density clearly show two humps. So earthquakes tend to either be
+# shallow or deep, and not much in-between.
+
+# A boxplot will not be appropriate for these data as we have two bumps!
+
+# Reason is that when we look at a boxplot, we 'see' the box. Which suggests to us
+# a single 'mode' or hump. But these data have two humps, so where the box is is actually
+# where the data aren't!
 ggplot(data=quakes) +
   geom_boxplot(mapping=aes(x=depth))
-# Boxplot completely inappropriate here, as we have two peaks. Generally
-# a boxplot should only be used if you have a single peak or hump (unimodal).
 
-# Otherwise, stick to a density or histogram.
-# This distribution has two humps - most earthquakes are either shallow (small depth)
-# or deep (large depth). Few are in between. So the measure of center here isn't
-# all that useful.
+# This is a bimodal distribution, so a boxplot will not be appropriate. Use a density
+# or histogram here.
 
-# Let's have a look at stations:
+# What about stations?
 ggplot(data=quakes) +
-  geom_histogram(mapping=aes(x=stations))
-ggplot(data=quakes) +
-  geom_density(mapping=aes(x=stations))
-ggplot(data=quakes) +
-  geom_boxplot(mapping=aes(x=stations))
-# This distribution is more skew than the others. Skew to the right (i.e. the tail
-# of extreme values is out to the right, and most data is to the small values).
-# Most eqarthquakes are felt at are around 25 stations.
+  geom_histogram(mapping=aes(x=stations), bins=20)
 
-# The outliers shown as individual points on the boxplot are determined by an
-# algorithm. Anything further than 1.5 interquartile ranges (length of box) away from
-# the ends of the box is considered extreme.
+ggplot(data=quakes) +
+  geom_density(mapping=aes(x=stations), adjust=1) +
+  geom_rug(mapping=aes(x=stations))
 
-# If you have just one or two with a big gap back to the next ones, it's worth checking
-# the data for coding errors. Otherwise, don't worry too much!
+# Stations is clearly skew to the right which makes sense as it's a count that plausibly
+# could be low (in this case I think it's at least 10 - that's probably data truncation
+# though!) so the counts can't be less than 10, so the data will tend to bunch up there.
+
+ggplot(data=quakes) +
+  geom_boxplot(mapping=aes(x=stations), fill='purple')
+
+# This is classic 'skew to the right' where extreme values tend to be only on the
+# right tail - most of the data are bunched on the left. Right hand tail longer than
+# the left hand tail, and the median is to the left of the box.
+
+
